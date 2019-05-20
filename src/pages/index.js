c85b0f0b28d3dcb87b21adf8f9f73e791762eed4
Whitney Bolar <wbolar@eagles.ewu.edu>
2019-05-20 14:06:13 -0700

    for nikky

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,17 @@
 import React from "react"
+import Header from "../components/header"
+import { Link } from "gatsby"
 
-export default () => <div>Hello world!</div>
+
+export default () => (
+  <div style={{ color: `teal` }}>
+
+    <Header headerText="Whitney Bolar" /> 
+    <Header headerText="Beetles" /> 
+    <Header headerText="Paul" /> 
+
+    <Link to="/contact/">Contact</Link>
+
+    <p>Such wow. Very React.</p>
+  </div>
+)
